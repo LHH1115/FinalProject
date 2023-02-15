9a84f0d16f2a8e670f25ee46100d7c3c30d08a13
LHH1115 <hohyung95@gmail.com>
2023-02-15 17:03:56 +0900

    Build: 기본 세팅 진행
    db 패키지 기본 작업 진행

---
+++ finalProject/src/main/java/com/example/demo/vo/accommodation/AccommodationVO.java
@@ -0,0 +1,18 @@
+package com.example.demo.vo.accommodation;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class AccommodationVO {
+
+	private int accommoNo;
+	private String name;
+	private String addr;
+	private String phone;
+	private int price;
+	private String category;
+}
