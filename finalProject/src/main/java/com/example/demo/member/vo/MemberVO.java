9881975802b0c698188cb2a9602556b7ac9677b6
sunghyunJ <shjay0306@gmail.com>
2023-03-02 14:56:44 +0900

    일단통합

---
+++ finalProject/src/main/java/com/example/demo/member/vo/MemberVO.java
@@ -0,0 +1,21 @@
+package com.example.demo.member.vo;
+
+import lombok.Data;
+
+@Data
+public class MemberVO {
+	private int memberno;
+	private String name;
+	private String jumin;
+	private String id;
+	private String pwd;
+	private String postcode;
+	private String addr;
+	private String email;
+	private String phone;
+	private int point;
+	private String role;
+	private String mbti;
+	private int roulettecount;
+
+}
